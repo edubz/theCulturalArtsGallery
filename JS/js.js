fb0e0ad01cd53a64a27cd0fc2599b04000a7f4f9
globali <ev4n.rost@gmail.com>
2019-03-16 10:13:53 -0700

    added content/navigation menu

---
+++ JS/js.js
@@ -0,0 +1,22 @@
+let hamburgerWrap = document.querySelector(".hamburger-wrap");
+let nav = document.querySelector(".navigation");
+let burger = document.querySelectorAll(".hamburger");
+
+
+
+hamburgerWrap.onclick = () => {
+  nav.classList.toggle("open");
+  for (var i = 0; i < burger.length; i++) {
+    burger[i].classList.toggle("open");
+  }
+}
+
+
+//why not working grrr!!!
+function myFunction() {
+    var w = window.innerWidth;
+    var h = window.outerHeight;
+    if(w > 450){
+      nav.classList.remove("open");
+    }
+}
