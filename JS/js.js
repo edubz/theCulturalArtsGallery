cd5afce99fe5e6082fc90fd014bb98ae83e5598a
Zavier Banks <zbanks@catalyte.io>
2019-03-16 12:02:00 -0700

    Added a new HTML file, and changed the main javascript a bit

--- JS/js.js
+++ JS/js.js
@@ -2,21 +2,24 @@ let hamburgerWrap = document.querySelector(".hamburger-wrap");
 let nav = document.querySelector(".navigation");
 let burger = document.querySelectorAll(".hamburger");
 
+window.onload = () => {
+    document.body.addEventListener('mousemove', myFunction);
+    hamburgerWrap.addEventListener('click', addingOpen);
 
-
-hamburgerWrap.onclick = () => {
-  nav.classList.toggle("open");
-  for (var i = 0; i < burger.length; i++) {
-    burger[i].classList.toggle("open");
-  }
 }
 
+function addingOpen() {
+    nav.classList.toggle("open");
+    for (var i = 0; i < burger.length; i++) {
+        burger[i].classList.toggle("open");
+    }
+}
 
 //why not working grrr!!!
 function myFunction() {
     var w = window.innerWidth;
     var h = window.outerHeight;
     if(w > 450){
-      nav.classList.remove("open");
+        nav.classList.remove("open");
     }
 }
